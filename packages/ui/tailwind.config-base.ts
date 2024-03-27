87bbff35a26e8b9ae834d5a49dc43f8d5c1c48d1
jramos0 <jonathan007rm@gmail.com>
2024-03-27 14:35:15 -0600

    Lexique Front End Part1

--- packages/ui/tailwind.config-base.ts
+++ packages/ui/tailwind.config-base.ts
@@ -210,6 +210,7 @@ const baseConfig = {
         'sm-card': '0px 0px 10px 0px #FF5C00',
         'sm-card-white': '0px 0px 10px 0px #FFFFFF',
         'button-white': '0px 0px 10px 0px rgba(255, 255, 255, 0.57)',
+        neon: '0 0 8px #FF5C0040, 0 0 16px #FF5C0040, 0 0 24px #FF5C0040',
       },
     },
   },
